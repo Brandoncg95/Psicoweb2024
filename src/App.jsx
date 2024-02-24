9b78d44044c7af279d17d885b1ba1aa17e64220d
Brandon <bcatanogarcia@gmail.com>
2024-02-23 22:36:00 -0500

    Se crea configuracion de proyecto en amplify

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>
+
+        </header>
+      </div>
+
+    </>
+  )
+}
+
+export default App
